13922a535a1e8549b7c4fb5e1c998b7afa14b210
Akshay singh <ww.akshay@gmail.com>
2020-07-17 12:52:09 +0530

    Updates

---
+++ precache-manifest.ba416683990d8103cf04a860f38a2d38.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af6bad462e885a070e8f2d8e5c59b574",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "2034afe583043adb016d",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "723c024c89ba066c538a",
+    "url": "/robofriends/static/css/main.4c13d235.chunk.css"
+  },
+  {
+    "revision": "2034afe583043adb016d",
+    "url": "/robofriends/static/js/2.b67b109c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.b67b109c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "723c024c89ba066c538a",
+    "url": "/robofriends/static/js/main.bea1491f.chunk.js"
+  },
+  {
+    "revision": "799091151d8d24875906",
+    "url": "/robofriends/static/js/runtime-main.c98a3899.js"
+  },
+  {
+    "revision": "2e5ec5b3e538df515284bcc246b0629d",
+    "url": "/robofriends/static/media/Strawberry Shortcake Supplement Logo.2e5ec5b3.woff"
+  }
+]);
